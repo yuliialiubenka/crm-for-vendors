4623104fb540977393bc58f1631dccb41b5eb658
Yuliia Liubenka <yuliialiubenka@gmail.com>
2024-04-27 17:08:09 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import HomePageHeader from './components/hp-header';
 import LottiePlayer from './components/lottie-player';
 import FeatureDesc from './components/feature-desc';
 
-export interface PageProps {}
+export interface PageProps { }
 
-export default function Home({}: PageProps) {
+export default function Home({ }: PageProps) {
 
   return (
     <>
-      <HomePageHeader/>
+      <HomePageHeader />
       <main className="container max-w-screen-xl flex items-center justify-between">
-        <LottiePlayer/>
-        <FeatureDesc/>
+        <LottiePlayer />
+        <FeatureDesc />
       </main>
     </>
   );
